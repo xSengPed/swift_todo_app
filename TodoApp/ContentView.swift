1fcb2b906e0084c10f0f0f59d1e80201162f540d
Donnukrit Satirakul <osacn_donnukrit.satirakul@krungthai.com>
2023-09-06 15:20:05 +0700

    Initial Commit

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Donnukrit Satirakul on 6/9/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("My Todo Application")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Donnukrit Satirakul on 6/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
